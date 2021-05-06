a4696535b3e100bf12f0e6b27dd8350285ce07e4
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2021-05-06 23:53:50 +0200

    Create function to return even int in given range

---
+++ random_lec5_exercise2.py
@@ -0,0 +1,9 @@
+import random
+def genEven():
+    '''
+    Returns a random even number x, where 0 <= x < 100
+    '''
+    # Your code here
+    return random.randrange(0, 100, 2)
+
+print(genEven())
