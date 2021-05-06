85cda2556d1ec153052c95a3df53e657dd3bf2e0
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2021-05-07 00:23:01 +0200

    Create function to return deterministic even int in given range

---
+++ random_lec5_exercise3.py
@@ -0,0 +1,13 @@
+import random
+def deterministicNumber():
+    '''
+    Deterministically generates and returns an even number between 9 and 21
+    '''
+    random.seed(0)
+    x = random.randrange(9, 21)
+    if x % 2 == 0:
+        return x
+    else:
+        return x + 1
+
+print(deterministicNumber())
